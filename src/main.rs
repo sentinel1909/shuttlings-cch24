e4f7edaae0a19b5740a3d6d48abe5f02041eb20f
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-12-03 19:40:06 -0800

    improvement: implement Day MInus 1, Task 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 // src/main.rs
 
 // dependencies
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{
+    http::{header, HeaderMap, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
 
 // Day -1, Task 1 handler
 async fn dayminus1_task1() -> impl IntoResponse {
     "Hello, bird!"
 }
 
+// Day -1, Task 2 handler
+async fn dayminus1_task2() -> impl IntoResponse {
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        header::LOCATION,
+        "https://www.youtube.com/watch?v=9Gc4QTqslN4"
+            .parse()
+            .unwrap(),
+    );
+    (headers, StatusCode::FOUND)
+}
+
 // main function
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(dayminus1_task1));
+    let router = Router::new()
+        .route("/", get(dayminus1_task1))
+        .route("/-1/seek", get(dayminus1_task2));
 
     Ok(router.into())
 }
