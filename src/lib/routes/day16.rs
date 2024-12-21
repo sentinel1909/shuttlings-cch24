9c9fd702ed6898b988a9ad296cab386694f27162
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-12-21 14:17:06 -0800

    improvement: Day 16, Task 1 completed and verified

--- src/lib/routes/day16.rs
+++ src/lib/routes/day16.rs
@@ -3,7 +3,7 @@
 // dependencies
 use axum::{extract::Json, http::StatusCode, response::IntoResponse};
 use axum_macros::debug_handler;
-use jsonwebtoken::{encode, EncodingKey, Header};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use serde_json::Value;
 use tower_cookies::{Cookie, Cookies};
 
@@ -11,7 +11,29 @@ use tower_cookies::{Cookie, Cookies};
 #[debug_handler]
 #[tracing::instrument(name = "Day 16, Task 1 Handler - Wrap Endpoint")]
 pub async fn day16_post_wrap(cookies: Cookies, Json(payload): Json<Value>) -> impl IntoResponse {
-    let jwt = encode(&Header::default(), &payload, &EncodingKey::from_secret(b"the_secret_key")).unwrap();
+    let jwt = encode(
+        &Header::default(),
+        &payload,
+        &EncodingKey::from_secret(b"the_secret_key"),
+    )
+    .unwrap();
     cookies.add(Cookie::new("gift", jwt));
     StatusCode::OK
+}
+
+// Day 16 Task 1 - unwrap endpoint
+#[debug_handler]
+#[tracing::instrument(name = "Day 16, Task 1 Handler - Unwrap Endpoint")]
+pub async fn day16_get_unwrap(cookies: Cookies) -> Result<Json<Value>, StatusCode> {
+    let ck = cookies.get("gift").ok_or(StatusCode::BAD_REQUEST)?;
+    let jwt = ck.value();
+    let mut validation = Validation::default();
+    validation.required_spec_claims.remove("exp");
+    let response_body = decode(
+        jwt,
+        &DecodingKey::from_secret(b"the_secret_key"),
+        &validation,
+    )
+    .unwrap();
+    Ok(Json(response_body.claims))
 }
--- src/lib/startup.rs
+++ src/lib/startup.rs
@@ -3,7 +3,7 @@
 // dependencies
 use crate::routes::day12::Game;
 use crate::routes::day12::{day12_get_board_state, day12_post_play_game, day12_post_reset_board};
-use crate::routes::day16::day16_post_wrap;
+use crate::routes::day16::{day16_post_wrap, day16_get_unwrap};
 use crate::routes::day2::{day2_task1, day2_task2};
 use crate::routes::day5::day5_task1;
 use crate::routes::day9::day9_tasks;
@@ -81,6 +81,7 @@ impl Application {
             .route("/12/reset", post(day12_post_reset_board))
             .route("/12/place/:team/:column", post(day12_post_play_game))
             .route("/16/wrap", post(day16_post_wrap))
+            .route("/16/unwrap", get(day16_get_unwrap))
             .with_state(state)
             .layer(CookieManagerLayer::new())
             .layer(
